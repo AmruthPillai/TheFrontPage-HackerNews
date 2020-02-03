3e0a9f60898eb6c4d6bcb8ef10ed3b2cb90743c7
Amruth Pillai <im.amruth@gmail.com>
2020-02-03 12:26:35 +0530

    connecting it with an api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Post from './components/Post/Post';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const topStories = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+      topStories.data.slice(0, 20).map(async (x) => {
+        const res = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${x}.json`);
+        setPosts((p) => [...p, res.data]);
+      });
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container-fluid mt-3">
       <Header />
@@ -10,7 +25,9 @@ function App() {
       <hr className="my-3" />
 
       <div className="container">
-        <Post upvoted />
+        {
+          posts.map((x) => <Post key={x.id} post={x} />)
+        }
       </div>
     </div>
   );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,41 +1,66 @@
 import React from 'react';
+import moment from 'moment';
 import './Post.css';
 
-import CaretUp from '../../assets/images/caret-up.png';
+// import CaretUp from '../../assets/images/caret-up.png';
 import CaretUpFilled from '../../assets/images/caret-up-filled.png';
 import HideIcon from '../../assets/images/hide.png';
 import CommentIcon from '../../assets/images/comment.png';
 
-function Post({ upvoted = false }) {
+function domainFromUrl(url) {
+  let result;
+  let match;
+  // eslint-disable-next-line no-cond-assign
+  if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?=]+)/im)) {
+    // eslint-disable-next-line prefer-destructuring
+    result = match[1];
+    // eslint-disable-next-line no-cond-assign
+    if (match = result.match(/^[^.]+\.(.+\..+)$/)) {
+      // eslint-disable-next-line prefer-destructuring
+      result = match[1];
+    }
+  }
+  return result;
+}
+
+function Post({ post }) {
   return (
     <div id="Post" className="d-flex align-items-center">
-      <div className="upvote d-flex flex-column justify-content-center align-items-center">
-        {
-          upvoted ? <img className="img-fluid" src={CaretUpFilled} alt="Upvoted" /> : <img className="img-fluid" src={CaretUp} alt="Upvoted" />
-        }
-        <span className="font-weight-bold">218</span>
+      <div className="upvote cursor-pointer d-flex flex-column justify-content-center align-items-center">
+        <img className="img-fluid" src={CaretUpFilled} alt="Upvoted" />
+        <span className="font-weight-bold">{post.score}</span>
       </div>
 
       <div className="meta ml-4">
-        <h4 className="mb-1 font-weight-bold">
-          Old CSS, New CSS
+        <a href={post.url} target="_blank" rel="noopener noreferrer">
+          <h4 className="mb-1 font-weight-bold">
+            {post.title}
+            {' '}
+            <small>
+              (
+              {domainFromUrl(post.url)}
+              )
+            </small>
+          </h4>
+        </a>
+        <h6 className="mb-0">
+          by
           {' '}
-          <small>(eeve.ee)</small>
-        </h4>
-        <h6 className="mb-0">by culturedsystems</h6>
+          {post.by}
+        </h6>
       </div>
 
       <div className="actions ml-auto d-flex align-items-center">
-        <div className="comment pr-5 d-flex flex-column align-items-center">
+        <div className="comment cursor-pointer pr-5 d-flex flex-column align-items-center">
           <img className="img-fluid" src={CommentIcon} alt="Comment on Post" />
-          <span className="font-weight-bold">23</span>
+          <span className="font-weight-bold">{post.descendants}</span>
         </div>
 
-        <div className="hide pr-5">
+        <div className="hide cursor-pointer pr-5">
           <img className="img-fluid" src={HideIcon} alt="Hide Post" />
         </div>
 
-        <div className="timestamp">4 hours ago</div>
+        <div className="timestamp text-right">{moment.unix(post.time).fromNow()}</div>
       </div>
     </div>
   );
